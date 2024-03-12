c73e0ca9a24fd7aa88e8fc3e079541f4171c533e
Vlad <hennessyfgh@gmail.com>
2024-03-12 16:33:46 +0300

    big correction

--- src/new_classes.py
+++ src/new_classes.py
@@ -2,16 +2,16 @@ from classes import Category, Product
 from abs_and_mixin import MixinRepr, AbsProd
 
 
-class Smartphone(Product, MixinRepr):
+class Smartphone(Product):
     performance: int  # производительность
     model: str  # модель
     memory_capacity: int  # объём встроенной памяти
 
     def __init__(self, name, description, price, color, quantity, performance, model, memory_capacity):
-        super().__init__(name, description, price, color, quantity)
         self.performance = performance
         self.model = model
         self.memory_capacity = memory_capacity
+        super().__init__(name, description, price, color, quantity)
 
 
     @classmethod
@@ -20,14 +20,14 @@ class Smartphone(Product, MixinRepr):
         return cls(name, description, price, quantity, color, performance, model, memory_capacity)
 
 
-class LawnGrass(Product, MixinRepr):
+class LawnGrass(Product):
     manufacturer_country: str  # страна-производитель
     germination_period: float  # срок прорастания
 
     def __init__(self, name, description, price, color, quantity, manufacturer_country, germination_period):
-        super().__init__(name, description, price, color, quantity)
         self.manufacturer_country = manufacturer_country
         self.germination_period = germination_period
+        super().__init__(name, description, price, color, quantity)
 
 
     @classmethod
